1c28a970740df0e3788cb87fa676933eda23e5a0
Billo Diallo <billodiallonet@gmail.com>
2021-10-05 13:22:23 +0300

    Fix button issue and add mobile size

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-const menuBars = document.getElementsByClassName('menu-bars')[0]
-const mobilemenu = document.getElementsByClassName('mobilemenu')[0]
-const header = document.getElementById('header')
+const menuBars = document.getElementsByClassName('menu-bars')[0];
+const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
+const header = document.getElementById('header');
 const closeBtn = document.getElementById('hamBtn');
 
+menuBars.addEventListener('click', () => {
+  if (mobileMenu.classList.contains('open')) {
+    mobileMenu.style.display = 'none';
+    closeBtn.className = 'fas fa-bars';
+    mobileMenu.classList.remove('open');
+    header.classList.remove('active');
+  } else {
+    mobileMenu.style.display = 'flex';
+    closeBtn.className = 'fas fa-times';
+    mobileMenu.classList.add('open');
+    header.classList.add('active');
+  }
+});
