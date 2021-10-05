b08c17b6d0baa04d48b2dbf9dab6cfc5de459ab1
Billo Diallo <billodiallonet@gmail.com>
2021-10-05 11:06:45 +0300

    Add mobile-menu,header,closeBtn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-const menuBars = document.getElementsByClassName('menu-bars')
+const menuBars = document.getElementsByClassName('menu-bars')[0]
+const mobilemenu = document.getElementsByClassName('mobilemenu')[0]
+const header = document.getElementById('header')
+const closeBtn = document.getElementById('hamBtn');
+
